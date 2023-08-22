695ec882632ae6fbf493c2c8a7f1856dfda0c3ad
Zain Ul Abideen <142613496+dominuszain@users.noreply.github.com>
2023-08-22 18:36:36 +0500

    Add files via upload

---
+++ main.f95
@@ -0,0 +1,35 @@
+program damped_harmonic_oscillator
+
+	implicit none
+	
+	! declaring the necessary variables.
+	real :: b, m, k, delt, v, x, t
+	integer :: n
+	
+	! defining the trivial values for constants.
+	b = 1
+	m = 1
+	k = 4
+	
+	v = 0
+	x = 1
+	delt = 0.001
+	
+	t = 0
+	
+	open(1, file="out.txt")
+	
+	! main loop
+	do n = 1, 10000
+		
+		v = v + ((-(b/m)*v)-(k/m)*x)*delt
+		x = x + v*delt
+		
+		t = t + delt
+		
+		write (1, *) t, x
+	end do
+	
+	close(1)
+	
+end program damped_harmonic_oscillator
